8976e43a61af20b6619c0a3d7f1389a4455c6d68
jvitoraway <jvitor-away@hotmail.com>
2020-07-12 22:50:28 -0300

    desafios concluidos

--- lib/services/quiz_api.dart
+++ lib/services/quiz_api.dart
@@ -6,7 +6,7 @@ import 'package:quiz_covid19/models/question.dart';
 class QuizApi {
   static Future<List<Question>> fetch() async {
     try {
-      var url = 'https://script.google.com/macros/s/AKfycbxI628yjt1RNE9ENfMs7QLRdZwUylRwUbdFwFSaMMvQtR01rfs/exec';
+      var url = 'https://script.googleusercontent.com/macros/echo?user_content_key=p_SHV-y9DuIib9Fp_w7UIZPa3EoccEsPtAtvE43DniKAlvXpJpMvU6uXpSfWDEWwxd8wjAue4OqRfvbFa_95krwM2cXTPNtxm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnLmtTavgXqWXyeELmXRb9ZvurCL_FsjE_OrD3yeKLY4hUz0SRt21jV3EvLs_ACBjDXXE9vqSRAli&lib=Mp4fhLrr5N2zIsW20sIBkzNzN4ag7LKMu';
       var response = await http.get(url);
 
       if (response.statusCode == 200) {
